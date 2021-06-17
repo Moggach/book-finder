723a51fd4d856e40369bfc2e3f70fdb3071953aa
Anna Cunnane <43313455+Moggach@users.noreply.github.com>
2021-06-17 17:18:11 +0100

    Delete CodeWars directory

--- CodeWars/HighestandLowest7kyu.js
+++
@@ -1,8 +0,0 @@
-function highAndLow(numbers){
-    let array = numbers.split(" ");
-    let sortedArray = array.sort((a, b) => b - a); 
-    let str1 = sortedArray[0] 
-    let str2 = sortedArray.slice(-1)[0] 
-    let result = str1 +  " " + str2
-    return result;
-   }
